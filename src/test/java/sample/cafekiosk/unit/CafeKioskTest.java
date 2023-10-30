b08c88bf783f4e0d56f7f80582ef80ce72474982
ing9990 <mail80782258@gmail.com>
2023-10-30 14:00:12 +0900

    test: Add automatic test

--- src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -17,4 +17,43 @@ class CafeKioskTest {
         System.out.println(">>> 담긴 음료 수: " + cafeKiosk.getBerverages().size());
         System.out.println(">>> 담긴 음료: " + cafeKiosk.getBerverages().get(0).getName());
     }
+
+
+    @Test
+    void add() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+
+        cafeKiosk.add(new Americano());
+
+        assertThat(cafeKiosk.getBerverages()).hasSize(1);
+        assertThat(cafeKiosk.getBerverages().get(0).getName()).isEqualTo("아메리카노");
+    }
+
+    @Test
+    void remove() {
+        CafeKiosk kiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        kiosk.add(americano);
+        assertThat(kiosk.getBerverages()).hasSize(1);
+
+        kiosk.remove(americano);
+        assertThat(kiosk.getBerverages()).isEmpty();
+    }
+
+    @Test
+    void clear() {
+        CafeKiosk kiosk = new CafeKiosk();
+        Americano americano = new Americano();
+        Latte latte = new Latte();
+
+        kiosk.add(americano);
+        kiosk.add(latte);
+
+        assertThat(kiosk.getBerverages()).hasSize(2);
+
+        kiosk.clear();
+        assertThat(kiosk.getBerverages()).hasSize(0);
+    }
+
 }
